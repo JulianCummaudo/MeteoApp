095910a929aa43b999a82baec72122fce9827130
Juj <julian.cummaudo@gmail.com>
2026-04-07 19:55:28 +0200

    Map is now clickable

--- MeteoApp/MapPage.xaml.cs
+++ MeteoApp/MapPage.xaml.cs
@@ -1,14 +1,24 @@
 using Microsoft.Maui.Controls.Maps;
 using Microsoft.Maui.Maps;
+using MeteoApp.Services;
+using MeteoApp.Models;
 
 namespace MeteoApp;
 
 public partial class MapPage : ContentPage
 {
+    private readonly MeteoService _meteoService = new MeteoService();
+
     public MapPage()
     {
 		InitializeComponent();
         InitializeMap();
+        MyMap.MapClicked += OnMapClickedWrapper;
+    }
+
+    private void OnMapClickedWrapper(object sender, MapClickedEventArgs e)
+    {
+        _ = OnMapClicked(sender, e);
     }
 
     private void InitializeMap()
@@ -29,5 +39,45 @@ public partial class MapPage : ContentPage
         // 3. Center the map around Lugano with a 1 km radius
         var region = MapSpan.FromCenterAndRadius(location, Distance.FromKilometers(1));
         MyMap.MoveToRegion(region);
+    }
+
+    public async Task OnMapClicked(object sender, MapClickedEventArgs e)
+    {
+        if (e.Location is Location location)
+        {
+            double latitude = location.Latitude;
+            double longitude = location.Longitude;
+
+            try
+            {
+                var meteo = await _meteoService.GetConditionsAsync(location);
+
+                // Temporary CityEntry
+                var cityEntry = new CityEntry
+                {
+                    Name = "Posizione selezionata",
+                    Country = "",
+                    Lat = latitude,
+                    Lon = longitude
+                };
+
+                var meteoCityEntry = new MeteoCityEntry
+                {
+                    City = cityEntry,
+                    Meteo = meteo
+                };
+
+                var navigationParameter = new Dictionary<string, object>
+                {
+                    { "CityEntry", meteoCityEntry }
+                };
+
+                await Shell.Current.GoToAsync("entrydetails", navigationParameter);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlertAsync("Errore", "Impossibile recuperare il meteo", "OK");
+            }
+        }
     }
 }

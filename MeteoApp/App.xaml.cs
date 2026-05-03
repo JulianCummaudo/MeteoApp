407722c94e9085f42707304870d98d7d347a7480
Techative <info@techative.ch>
2026-05-03 10:51:07 +0200

    Removed tap method

--- MeteoApp/App.xaml.cs
+++ MeteoApp/App.xaml.cs
@@ -21,39 +21,5 @@ public partial class App : Application
 		CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.CurrentUICulture;
 
 		MainPage = new AppShell();
-
-		// Add click notification handler
-		LocalNotificationCenter.Current.NotificationActionTapped += OnNotificationTapped;
-	}
-
-	private async void OnNotificationTapped(NotificationActionEventArgs e)
-	{
-		Debug.WriteLine($"Notification tapped: {e.Request.ReturningData}, IsDismissed: {e.IsDismissed}, IsTapped: {e.IsTapped}");
-
-		if (e.IsDismissed || e.IsTapped == false)
-			return;
-
-		int cityId = int.Parse(e.Request.ReturningData);
-		var city = await _databaseService.GetEntryByIdAsync(cityId);
-
-		if (city == null)
-			return;
-
-		var location = new Location(city.Lat, city.Lon);
-		var meteo = await _meteoService.GetConditionsAsync(location);
-
-		var entry = new MeteoCityEntry
-		{
-			City = city,
-			Meteo = meteo
-		};
-
-		await MainThread.InvokeOnMainThreadAsync(async () =>
-		{
-			await Shell.Current.GoToAsync("entrydetails", new Dictionary<string, object>
-			{
-				{ "CityEntry", entry }
-			});
-		});
 	}
 }

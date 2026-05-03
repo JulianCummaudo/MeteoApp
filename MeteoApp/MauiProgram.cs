fc021a362b9c243eefae896e794b178d62b12ae4
Juj <julian.cummaudo@gmail.com>
2026-05-03 12:21:34 +0200

    blazor works

--- MeteoApp/MauiProgram.cs
+++ MeteoApp/MauiProgram.cs
@@ -1,6 +1,6 @@
 ﻿using MeteoApp.ViewModels;
 using Microsoft.Extensions.Logging;
-
+using MeteoApp.Services;
 namespace MeteoApp;
 
 public static class MauiProgram
@@ -29,6 +29,7 @@ public static class MauiProgram
 
 		// Blazor
 		builder.Services.AddMauiBlazorWebView();
+		builder.Services.AddSingleton<MeteoService>();
 
 #if DEBUG
 		builder.Logging.AddDebug();
